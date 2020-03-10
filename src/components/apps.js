b2e505b2cc96738d12b8b470e1f4e1926f164e48
ndo@ndo1 <yo@ndo.dev>
2020-03-10 16:50:23 +0100

    fix: document SSR

--- src/components/apps.js
+++ src/components/apps.js
@@ -82,10 +82,12 @@ const getCategoryLabels = (data) => {
       </Tab>
     )
   })
-  if (typeof window !== 'undefined' && window.innerWidth > 768) {
-    appJsonArray.push(
-      <Cmd />
-    )
+  if (typeof window !== 'undefined') {
+    if (window.innerWidth > 768) {
+      appJsonArray.push(
+        <Cmd />
+      )
+    }
   }
   return appJsonArray
 }
